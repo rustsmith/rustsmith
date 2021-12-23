2186d7f504c817ddb73d2d2da72337e21fc72037
Mayank Sharma <mayank.sharma.uk@gmail.com>
2021-12-23 23:47:27 +0000

    Update install scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,14 +16,28 @@ repositories {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "15"
 }
-
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.6.0")
     implementation("com.github.ajalt.clikt:clikt:3.3.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+}
+
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes("Main-Class" to "com.rustsmith.MainKt")
+    }
+    from(configurations.compileClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+    exclude("**/module-info.class")
 }
 
 application {
+    applicationName = "rustsmith"
     mainClass.set("com.rustsmith.MainKt")
+}
+
+
+kotlinter{
+    disabledRules = arrayOf("no-wildcard-imports")
 }
